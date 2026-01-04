40e85a3c67065da9cf14dee02e38dd04d9ff092d
md <texet0598@gmail.com>
2026-01-04 15:08:56 +0300

    feat: rezise background

--- src/game.rs
+++ src/game.rs
@@ -1,9 +1,13 @@
-use bevy::{audio::AudioPlugin, prelude::*};
+use bevy::{prelude::*, window::WindowResized};
+
+#[derive(Component)]
+struct Background;
 
 pub fn startup() {
     App::new()
-        .add_plugins(DefaultPlugins.build().disable::<AudioPlugin>())
+        .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
+        .add_systems(Update, update)
         .run();
 }
 
@@ -19,14 +23,26 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>, windows: Query<
             image_mode: SpriteImageMode::Tiled {
                 tile_x: true,
                 tile_y: false,
-                stretch_value: 1.0,
+                stretch_value: 1.5,
             },
             ..default()
         },
         Transform::from_translation(pos),
+        Background,
     ));
     commands.spawn((
         Camera2d,
         Transform::default().looking_at(Vec3::ZERO, Vec3::Y),
     ));
+}
+
+fn update(mut backgrounds: Query<&mut Sprite, With<Background>>, windows: Query<&Window>) {
+    let window = windows.single().unwrap();
+    let window_size = Vec2::new(window.width(), window.height());
+
+    for mut sprite in &mut backgrounds {
+        if sprite.custom_size != Some(window_size) {
+            sprite.custom_size = Some(window_size);
+        }
+    }
 }
